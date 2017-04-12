bfdf03c7d73f4f480b7084c846fe287096685948
Scotty0002 <scottclan0002@gmail.com>
2017-04-12 12:45:52 +1200

    Update reverseSlow.cpp

--- reverseSlow.cpp
+++ reverseSlow.cpp
@@ -4,10 +4,13 @@
 #include "E101.h"
 
 int main(){
+	int read=read_analog(0);
+	if(read>4){
 	init();
 	set_motor(1,-75);
 	set_motor(2,75);
 	sleep1(1,0);
 	stop(1);
 	stop(2);
-	return 0;}
+	}
+		return 0;}
